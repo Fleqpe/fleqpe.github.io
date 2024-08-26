20f863ff0e0df0b5f2a8f0ff78e0173a0c0866eb
Fleqpe <bariskurt2002@hotmail.com>
2024-08-26 11:00:01 +0300

    updated website

---
+++ games-data.js
@@ -0,0 +1,75 @@
+const games = [
+  {
+    title: "Fantasy Idle Tycoon 2",
+    imageUrl: "images/fantasy-idle-tycoon2.png",
+    description:
+      "Fantasy Idle Tycoon is an idle game where you must build yourself a blacksmith empire by managing your blacksmiths in this fantasy universe! Expand your empire by managing your investors.",
+    pokiUrl: "https://poki.com/tr/g/fantasy-idle-tycoon-2",
+    googlePlayUrl: "",
+    crazyGamesUrl: "",
+  },
+  {
+    title: "Crafters Inc: Tycoon Empire",
+    imageUrl: "images/crafters-inc.png",
+    description:
+      "Crafters Inc: Tycoon Empire is an idle game where you craft amazing items like weapons and armor. Hire helpers to boost production and sell your creations to expand your team and workshop. Keep crafting and growing your empire to create even cooler things!",
+    pokiUrl: "https://poki.com/tr/g/crafters-inc-tycoon-empire",
+    googlePlayUrl: "",
+    crazyGamesUrl: "https://www.crazygames.com/game/crafters-inc-tycoon-empire",
+  },
+  {
+    title: "Miner Madness Tycoon",
+    imageUrl: "images/miner-madness.png",
+    description:
+      "Miner Madness Tycoon is an immersive game where you build a mining empire across time. Gather resources, amass wealth, and conquer the industry with engaging gameplay. Unleash your entrepreneurial skills and become the ultimate mining tycoon!",
+    pokiUrl: "",
+    googlePlayUrl: "",
+    crazyGamesUrl: "https://www.crazygames.com/game/time-to-mine---idle-tycoon",
+  },
+  {
+    title: "Slicer Cursor Idle",
+    imageUrl: "images/slicer-cursor.png",
+    description:
+      "Slicer Cursor is a unique idle game where your cursor becomes a slicing machine. Slice enemies to level up and boost your slicing power. Enjoy the blend of action and strategy as you upgrade your cursor for maximum impact in this casual, incremental game!",
+    pokiUrl: "",
+    googlePlayUrl: "",
+    crazyGamesUrl: "https://www.crazygames.com/game/time-to-mine---idle-tycoon",
+  },
+  {
+    title: "Fantasy Idle Tycoon ",
+    imageUrl: "images/fantasy-idle-tycoon.png",
+    description:
+      "Fantasy Idle Tycoon is an idle game where you must build yourself a blacksmith empire by managing your blacksmiths in this fantasy universe! Expand your empire by managing your investors.",
+    pokiUrl: "",
+    googlePlayUrl: "",
+    crazyGamesUrl: "https://www.crazygames.com/game/fantasy-idle-tycoon",
+  },
+  {
+    title: "Fishing Society",
+    imageUrl: "images/fishing-society.png",
+    description:
+      "Fishing Society lets you set sail, catch fish, and trade your haul for new gear and upgrades. Build your collection, explore the waters, and become the ultimate angler in this relaxing and rewarding adventure!",
+    pokiUrl: "",
+    googlePlayUrl: "",
+    crazyGamesUrl: "",
+  },
+  {
+    title: "Joe The Necromancer",
+    imageUrl: "images/joe-the-necromancer.png",
+    description:
+      "In Joe the Necromancer, protect your character by leveling up your necromancy skills. With simple gameplay, focus on upgrading abilities to fend off enemies and survive the dangers of the dark arts!",
+    pokiUrl: "",
+    googlePlayUrl: "",
+    crazyGamesUrl: "",
+  },
+  {
+    title: "Antea: Crafting Simulator",
+    imageUrl: "images/antea.png",
+    description:
+      "In Antea the Crafting Simulator, craft items through specialized buildings and earn money to upgrade them. Level up your structures to boost efficiency and dominate the crafting world!",
+    pokiUrl: "",
+    googlePlayUrl: "",
+    crazyGamesUrl: "",
+  },
+  // Add more games as needed
+];
